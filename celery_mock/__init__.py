e5ae57eb7eb252b507dd7aff35719251f57bd7d4
maiiku <mko_san@lafiel.net>
2020-10-13 14:37:45 +0200

    use cfg file instead of setup.py

--- celery_mock/__init__.py
+++ celery_mock/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = """Konrad Rotkiewicz"""
 __email__ = 'konrad@ulam.io'
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 
 from .celery_mock import task_mock  # noqa

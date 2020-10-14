05a5ec21798037e274015e30158df0e0695b9c7d
maiiku <mko_san@lafiel.net>
2020-10-14 08:20:50 +0200

    switch to poetry from setuptools

--- celery_mock/__init__.py
+++ celery_mock/__init__.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
 __author__ = """Konrad Rotkiewicz"""
 __email__ = 'konrad@ulam.io'
-__version__ = '1.1.5'
 
 from .celery_mock import task_mock  # noqa
